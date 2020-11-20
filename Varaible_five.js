09b3706af122b004d93a9190cea3384f600b32e9
VSRDVV <74640120+VSRDVV@users.noreply.github.com>
2020-11-20 16:08:50 +0700

    Add files via upload

---
+++ Varaible_five.js
@@ -0,0 +1,6 @@
+/*Declare variables and assign string, boolean, undefined and null data types*/
+
+var str = 'Hello World'; // string value
+var bool = true; // boolean value
+var undef; // no value assgined so it is undefined
+var nul = null; // NULL assigned
