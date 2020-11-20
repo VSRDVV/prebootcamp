c0fb1a6afad24ee5b89d28825f8530242201a325
VSRDVV <74640120+VSRDVV@users.noreply.github.com>
2020-11-20 15:46:33 +0700

    Delete Variables_fourth.js

--- Variables_fourth.js
+++
@@ -1,6 +0,0 @@
-/*Declare variables and assign string, boolean, undefined and null data types*/
-
-var str = 'Hello World'; // string value
-var bool = true; // boolean value
-var undef; // no value assgined so it is undefined
-var nul = NULL; // NULL assigned
