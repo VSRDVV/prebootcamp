91abd0eaf6bc74c554363bd83207645fa28dd1ad
VSRDVV <74640120+VSRDVV@users.noreply.github.com>
2020-11-20 16:05:24 +0700

    Add files via upload

---
+++ Variables.seven.js
@@ -0,0 +1,13 @@
+/*Write 6 statement which provide truthy & falsey values*/
+
+var a = 5 ;
+var b = 4;
+
+console.log(a<b);
+console.log(a>b);
+console.log(a==b);
+console.log(a!=b);
+console.log(1==0);
+console.log(1==1);
+console.log('a'!='A');
+console.log('a'=='a');
---
+++ Variables_six.js
@@ -0,0 +1,15 @@
+/*Convert the string to integer
+
+    parseInt()
+    Number()
+    Plus sign(+)*/
+    
+    
+    var str = '10';
+    console.log(str);
+    var n1 = parseInt(str);
+    console.log(n1);
+    var n2 = +str;
+    console.log(n2);
+    var n2 = Number(str);
+    console.log(n3);
