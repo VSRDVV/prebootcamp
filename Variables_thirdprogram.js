1f1947077c66f283d7f706958b4fae1225622b36
VSRDVV <74640120+VSRDVV@users.noreply.github.com>
2020-11-20 15:34:23 +0700

    Add files via upload

---
+++ Variables_thirdprogram.js
@@ -0,0 +1,7 @@
+/* Declare variables to store your first name, last name, marital status, country and age in multiple lines*/
+
+var first_name;
+var last_name;
+var marital_status;
+var country;
+var age;
