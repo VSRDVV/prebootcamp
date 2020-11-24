063c7e0d73871c3622b9f7b8d7efe7185dcc2ecf
VSRDVV <74640120+VSRDVV@users.noreply.github.com>
2020-11-24 15:21:52 +0700

    Add files via upload

---
+++ obkect_eight.js
@@ -0,0 +1,10 @@
+ let details = {'Shiva':1,'Maa':[2,3,4,5],'Sree':{'Age':21,'Income':0},'Sas':6,'Max':9,'One':3,'Tw0':6};
+  console.log(details['Sree']);
+  console.log(details['Maa']);
+  console.log(details);
+  
+  let car = {'color':['Red','Green','Blue','Orange','Purple'],'enginemodel':[1000,1200,1300],'model':'24ki','
+finoption':{'Available':['Yes','No']},'price':'cheap','segment':'luxury','review':'good'};
+  console.log(car);
+  console.log(car['color']);
+  console.log(car['finoption'].Available);
